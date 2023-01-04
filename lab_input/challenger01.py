584d1dd24a2442032fe1c5c08a418cf495cdcb32
mrjoelee <joe.05.lee@outlook.com>
2023-01-04 14:43:52 +0000

    challenge-print-input

---
+++ lab_input/challenger01.py
@@ -0,0 +1,17 @@
+
+
+def main():
+        
+        ##Ask user's name
+        user_name = input("what's your name?\n")
+
+        ##Ask the day of the week
+        dayOfWeek = input("what day is it?\n")
+
+        ##print statement
+        print("Hello, " + user_name + "!" + " Happy " + dayOfWeek, sep="") 
+
+        
+
+main()
+
