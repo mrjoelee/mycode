458dfd2148b47982d36e7ab5346845bd330d4bf9
mrjoelee <joe.05.lee@outlook.com>
2023-01-05 20:36:30 +0000

    learning about conditional if

---
+++ iftest/condition02.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+if else 
+"""
+def main():
+    """runtime"""
+
+    #asking user for hostname
+    hostname = input("What value should we set for hostname?")
+    
+    # checking if hostname is equals to MTG
+    if hostname == "MTG":
+
+        #if true then:
+        print("The hostname was found to be MTG")
+
+if __name__ == "__main__":
+    main()
