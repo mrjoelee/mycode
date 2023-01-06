46cb241d719981f19eaa030dc560e32b1abb3ebe
mrjoelee <joe.05.lee@outlook.com>
2023-01-06 21:59:01 +0000

    Looping techniques

---
+++ fact/looping02b.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Joe | joe.lee@tlgcohort.com
+   For - Looping across a file opened with 'with'"""
+def main():
+
+    # open file in read mode
+    with open("dnsservers.txt", "r") as dnsfile:
+        # indent to keep the dnsfile object open
+        # create list of lines
+        dnslist = dnsfile.readlines()
+        # loop over lines
+        for svr in dnslist:
+            #print and end without a newline
+            print(svr, end="")
+    
+    # no need to close our file - closed automatically
+if __name__ == "__main__":
+    main()
