4c471affa49261fcddbe9301ec4a0e5b21c12d60
mrjoelee <joe.05.lee@outlook.com>
2023-01-07 00:05:22 +0000

    updated challengeFarmLoops

--- lab_input/challenges/challengeFarmLoops.py
+++ lab_input/challenges/challengeFarmLoops.py
@@ -4,26 +4,39 @@ Joe | joe.lee@tlgcohort.com
 "Looping with for" Challenge (Farm Loops)"""
 
 def main():
-    farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
-         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
-         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+    """runtime"""
+    #creating a list of farms
+    #farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         #{"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         #{"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
 
+    farms = [{"name": "Southwest Farm", "agriculture": ["chickens", "carrots", "celery"]},
+         {"name": "Northeast Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "East Farm", "agriculture": ["bananas", "apples", "oranges"]},
+         {"name": "West Farm", "agriculture": ["pigs", "chickens", "llamas"]}]
+
+
+    #storing animals from NE Farm
     NE_animals = farms[0]["agriculture"]
-    SE_animals = farms[0]["agriculture"]
-    W_animals = farms[0]["agriculture"]
 
-    print("NE Farm")
-    for farm in NE_animals:
-        print(f"{farm}")
-
-    for farm in farms:
-        print("-", farm["name"])
-        choose = input("Pick a farm!\n")
+    #displaying all the animals from NE Farm
+    for animal in NE_animals:
+        print(f"{animal}")
     
-    if farm["name"].lower() == choose.lower():
-        for farm in farms:
-            print(f"farms[{choose}]['agriculture']")
+    #asking user to choose a farm
+    choose = input(f"What farm would like to choose ({farms[0]['name']},{farms[1]['name']},{farms[2]['name']})\n").lower()
 
+    for farm in farms: 
+        if farm["name"].lower() == choose:
+            #print(f"{farm['agriculture']}")
+            
+            agriculture = farm['agriculture']
+
+            for item in agriculture:
+                if item == "celery" or item == "carrots":
+                    continue
+                else:
+                    print(item)
 
 if __name__ == "__main__":
     main()
