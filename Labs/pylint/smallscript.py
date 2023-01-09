d948c0d637e8f7fa72d3f6035d24773479177249
mrjoelee <joe.05.lee@outlook.com>
2023-01-09 18:23:48 +0000

    Best practice and pylint

---
+++ Labs/pylint/smallscript.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Joe | joe.lee@tlgcohort.com
+
+Best Practice and pylint
+"""
+
+
+def main():
+    """runtime"""
+    mynote = "print this string" # define mynote
+    print(mynote)                # display mynote
+
+if __name__ =="__main__":
+    main()
