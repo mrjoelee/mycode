cb6175367436e498460ff8c0e099982c24537399
mrjoelee <joe.05.lee@outlook.com>
2023-01-09 20:28:45 +0000

    challengesimplycode

--- challenges/challengeList.py
+++ challenges/challengeList.py
@@ -11,7 +11,7 @@ def main():
 
     #creating a list of students
     tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
-    
+
     #adding 4 to the list of wordbank
     wordbank.append(4)
 
@@ -33,14 +33,14 @@ def main():
 
     # setting the num from input to identify the student from tlgstudent list
     student_name = tlgstudents[num]
-    
+
     print(f"{student_name} always uses {wordbank[2]} {wordbank[1]} to indent")
 
 
     #Bonus 1 - randomly choose a student
     random_num = random.randint(0,20)
     random_student = tlgstudents[random_num]
-    
+
     #printing random student
     print(f"{random_student}")
 
