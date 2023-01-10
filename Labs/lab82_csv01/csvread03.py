775fcdc1793fac38fe3d716f4d79d11422f6c8c2
mrjoelee <joe.05.lee@outlook.com>
2023-01-10 19:13:12 +0000

    csv and pandas

---
+++ Labs/lab82_csv01/csvread03.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Joe | joe.lee@tlgcohort.com
+writing a dictionary and defining keys and values
+"""
+import csv
+
+def main():
+    with open('inventory.csv', mode='w') as csv_file:
+        fieldnames = ['hostname', 'ip', 'service']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'hostname': 'dumbledore', 'ip': '192.168.9.22', 'service': 'objectstorage'})
+        writer.writerow({'hostname': 'hermione', 'ip': '10.0.2.66', 'service': 'httpd'})
+if __name__ == "__main__":
+    main()
+
---
+++ Labs/lab82_csv01/csvread03panda.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Joe | joe.lee@tlgcohort.com
+using pandas to change data (list) into a csv
+"""
+
+import pandas
+
+def main():
+
+    ## create a python list
+    mydata = [
+    {'hostname': 'dumbledore', 'ip': '192.168.9.22', 'service': 'objectstorage'},
+    {'hostname': 'hermione', 'ip': '10.0.2.66', 'service': 'httpd'}
+    ]
+
+    ## create a data frame from our python data
+    df = pandas.DataFrame(mydata)
+
+    ## create the csv file without the index labels
+    df.to_csv("inventorypandas.csv", index=False)
+
+if __name__ == "__main__":
+    main()
+
