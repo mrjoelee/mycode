5869dd27074c3e41d13ae8c05ba672b1e3d6d1dc
mrjoelee <joe.05.lee@outlook.com>
2023-01-10 20:42:52 +0000

    Learning to work with the matplotlib library

---
+++ Labs/graphing/graphmaker-bar.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Joe | joe.lee@tlgcohort.com
+Producing Graph
+"""
+#using numpy by alias 'np'
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+def main():
+    """runtime"""
+    N = 4
+    localnetMeans = (20, 35, 30, 35) #LAN length of outage (mins)
+    wanMeans = (25, 32, 34, 20) #WAN length of outage (min)
+    ind = np.arange(N)    # the x locations for the groups
+    # the width of the bars: can also be len(x) sequence
+    width = 0.35
+
+    # describe where to display p1
+    p1 = plt.bar(ind, localnetMeans, width)
+    # stack p2 on top of p1
+    p2 = plt.bar(ind, wanMeans, width, bottom=localnetMeans)
+
+    # Describe the table metadata
+    plt.ylabel("Length of Outage (mins)")
+    plt.title("2018 Network Summary")
+    plt.xticks(ind, ("Q1", "Q2", "Q3", "Q4"))
+    plt.yticks(np.arange(0, 81, 10))
+    plt.legend((p1[0], p2[0]), ("LAN", "WAN"))
+
+    # display the graph
+    # plt.show() # you can try this on a Python IDE with a GUI if you'd like
+    plt.savefig("/home/student/mycode/Labs/graphing/2018summary.png")
+    # save a copy to "~/static" (the "files" view)
+    plt.savefig("/home/student/static/2018summary.png")
+
+
+if __name__ == "__main__":
+    main()
+
